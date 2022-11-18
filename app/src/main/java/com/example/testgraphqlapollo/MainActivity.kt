071b759cdca8021146f0ca8d966b52b02451911d
chenzhizsqq <chenzhi@qq.com>
2022-11-18 18:18:37 +0900

    暂时配置好

--- app/src/main/java/com/example/testgraphqlapollo/MainActivity.kt
+++ app/src/main/java/com/example/testgraphqlapollo/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.testgraphqlapollo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.retryWhen
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
